6e07efc4e9f21b0ef1b65e7575b2e17d63ffd1a9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-03-23 20:46:50 -0300

    chore: base action

---
+++ Cae.Utils.Trier/Actions/Action.cs
@@ -0,0 +1,33 @@
+namespace Cae.Utils.Trier.Actions;
+
+public abstract class Action<T,TO>
+{
+    public TO Execute(T input)
+    {
+        try
+        {
+            return ExecuteInternalAction(input);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    public Task<TO> ExecuteAsync(T input)
+    {
+        try
+        {
+            return ExecuteInternalActionAsync(input);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
+    protected abstract TO ExecuteInternalAction(T input);
+    protected abstract Task<TO> ExecuteInternalActionAsync(T input);
+}
