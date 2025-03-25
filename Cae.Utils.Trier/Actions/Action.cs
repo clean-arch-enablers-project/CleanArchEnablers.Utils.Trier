9acf43d25a02b933c97045c088a78a3d23324628
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-03-25 08:04:09 -0300

    fix: action cant try something

--- Cae.Utils.Trier/Actions/Action.cs
+++ Cae.Utils.Trier/Actions/Action.cs
@@ -1,33 +1,17 @@
 namespace Cae.Utils.Trier.Actions;
 
-public abstract class Action<T,TO>
+public abstract class Action<T, TO>
 {
     public TO Execute(T? input = default)
     {
-        try
-        {
-            return ExecuteInternalAction(input);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        return ExecuteInternalAction(input);
     }
 
     public Task<TO> ExecuteAsync(T? input = default)
     {
-        try
-        {
-            return ExecuteInternalActionAsync(input);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        return ExecuteInternalActionAsync(input);
     }
-    
+
     protected abstract TO ExecuteInternalAction(T input);
     protected abstract Task<TO> ExecuteInternalActionAsync(T input);
 }
