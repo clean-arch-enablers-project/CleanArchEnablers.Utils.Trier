cd2603ae201caa92317be3fdf645062501b2b2ac
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-03-27 21:33:18 -0300

    chore: adding exceptions to builder

--- Cae.Utils.Trier/TrierBuilder.cs
+++ Cae.Utils.Trier/TrierBuilder.cs
@@ -9,10 +9,21 @@ public class TrierBuilder<T,TO>
     private readonly Actions.Action<T, TO> _action;
     private readonly T? _input;
 
+    private List<Exception> ExceptionsToRetry { get; set; } = []; 
+    private int MaxAmountOfRetries { get; set; } = 0;
+
     public TrierBuilder(Actions.Action<T, TO> action, T? input)
     {
         _action = action;
         _input = input;
+    }
+
+    public TrierBuilder<T, TO> AutoRetryOn<TE>(int maxAmountOfRetries) where TE : Exception, new()
+    {
+        MaxAmountOfRetries = maxAmountOfRetries;
+        ExceptionsToRetry.Add(new TE());
+        
+        return this;
     }
 
     public Trier<T,TO> WithUnexpectedExceptionHandler(IUnexpectedExceptionHandler unexpectedExceptionHandler)
